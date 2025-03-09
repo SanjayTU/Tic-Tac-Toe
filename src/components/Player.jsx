c9ba8b443cbb431e9e88261e3fd2ef578bc73b23
Sanjay <sanjayullas4@gmail.com>
2025-03-09 19:35:17 +0530

    AddedGameboard,player switching

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export default function Player({initialName,symbol})
+export default function Player({initialName,symbol,isActive})
 {
 
     const [playerName, setPlayerName] = useState(initialName);
@@ -9,15 +9,21 @@ export default function Player({initialName,symbol})
     function handleEditClick(){
         setIsEditing((editing) => !editing);
     }
-
-    let editablePlayerName = <span className="player-name">{initialName}</span>;
-
-    if(isEditing){
-        editablePlayerName = <input type="text" required value={initialName}/>;
+    
+    function handleChange(event){
+        setPlayerName(event.target.value);
     }
 
+    let editablePlayerName = <span className="player-name">{playerName}</span>;
+
+    if(isEditing){
+        editablePlayerName = <input type="text" required value={playerName} 
+        onChange={handleChange}/>;
+    }
+
+
     return (
-        <li>
+        <li className={isActive ? 'active' : undefined}>
             <span className="player">
                 {editablePlayerName}
                 <span className="player-symbol">{symbol}</span>
