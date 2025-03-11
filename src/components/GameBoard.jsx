34fe53e753c1a39c50a6adbf58539897e96fa1fa
Sanjay <sanjayullas4@gmail.com>
2025-03-11 12:21:49 +0530

    Added game finish logic,rematch,and addressed some bugs

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,33 +1,26 @@
 
 
-const initialGameBoard = [
-    [null,null,null],
-    [null,null,null],
-    [null,null,null],
-]
 
-export default function GameBoard({ onSelectSquare,turns }){
 
-    let gameboard = initialGameBoard;
-    
-    for(const turn of turns){
-        const {square,player} = turn;
-        const {row,col} = square;
+export default function GameBoard({ onSelectSquare,board }){
 
-        gameboard[row][col] = player;
-    }
+  
 
     return (
 
         <ol id="game-board"> 
-          {gameboard.map((row,rowIndex) => (<li key={rowIndex}>
-
-            <ol>
-                {row.map((playerSymbol,colIndex) => ( 
-                <li key={colIndex}>
-                    <button onClick={() => onSelectSquare(rowIndex,colIndex)}>{playerSymbol}</button> 
-                </li>))}
-            </ol>
+          {board.map((row,rowIndex) => (
+            <li key={rowIndex}>
+                <ol>
+                {
+                 row.map((playerSymbol,colIndex) => ( 
+                 <li key={colIndex}>
+                    <button onClick={() => onSelectSquare(rowIndex,colIndex)} disabled={playerSymbol !== null}>
+                       {playerSymbol}  
+                    </button> 
+                 </li>))
+                }
+                </ol>
           </li>))}
         </ol>
     );
